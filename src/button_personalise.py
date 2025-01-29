8b25fdb3d00c49138d189ca2955acaf86231fac4
HertzyTambwe <hertytambwe@gmail.com>
2025-01-29 12:21:57 +0200

    personalisation des controles

--- src/button_personalise.py
+++
@@ -1,8 +0,0 @@
-import flet as ft
-
-class Mybutton(ft.ElevatedButton):
-    def __init__(self, text):
-        super().__init__()
-        self.bgcolor= ft.Colors.ORANGE_300
-        self.color = ft.Colors.GREEN_800
-        self.text = text
--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
-import time
+
 import flet as ft
 
 
-def main(page: ft.Page):
-    
-    def checkbox_changed(e):
-        output_text.value =(
-            f"Tu as apris a comment skyer {todo_check.value}"
-        )
-        page.update()
+class Mybutton(ft.ElevatedButton):
+    def __init__(self, text, on_click):
+        super().__init__()
+        self.bgcolor= ft.Colors.ORANGE_300
+        self.color = ft.Colors.GREEN_800
+        self.text = text
+        self.on_click = on_click
 
-    output_text = ft.Text()
-    todo_check = ft.Checkbox(label="Todo: Apprendre a faire du sky", value=False, on_change=checkbox_changed)
-    page.add(todo_check, output_text)
+def main(page: ft.Page):
+
+    def ok_clickecd(e):
+        """methode pour ok"""
+        print("Ok i'm go")
+    
+    def cancel_clicked(e):
+        """Methode pour cancel"""
+        print("Sorry bro your mon it not ok.")
+
+    page.add(Mybutton(text="OK it work", on_click=ok_clickecd), Mybutton(text="cancel", on_click=cancel_clicked))
+    
+
 ft.app(main)
